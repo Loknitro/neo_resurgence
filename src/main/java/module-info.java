7c9c829360e801054364ff6949ed68e4fe7961b7
loknitro <leo-vecchi@outlook.com>
2025-05-20 17:01:04 -0300

    Compiling issues fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,6 @@
 module com.loknitro.neo_resurgence {
     requires javafx.controls;
     requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-    requires java.desktop;
     requires javafx.media;
 
     opens com.loknitro.neo_resurgence to javafx.fxml;
